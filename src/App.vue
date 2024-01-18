e00ee280123e9aebbdceaa33dc75d147bb901bb9
mwmzuishuai <2713459649@qq.com>
2024-01-18 09:03:01 +0800

    整合

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
 <template>
   <div>
-    <Test></Test>
+    <!-- <Test></Test> -->
+    <div class="box1111">
+      <a-tag color="pink"><router-link to="/jiazai">加载</router-link></a-tag>
+      <a-tag color="red"><router-link to="/caisedanmu">彩色弹幕</router-link></a-tag>
+      <a-tag color="orange"><router-link to="/daoying">倒影</router-link></a-tag>
+      <a-tag color="green"><router-link to="/diqiuxuanzhuan">地球旋转</router-link></a-tag>
+      <a-tag color="cyan"><router-link to="/jianbianxuanting">渐变按钮悬停效果</router-link></a-tag>
+      <a-tag color="blue"><router-link to="/lianyi">涟漪效果</router-link></a-tag>
+      <a-tag color="purple"><router-link to="/shanyao">闪耀弹幕</router-link></a-tag>
+      <a-tag color="pink"><router-link to="/shubiaoxuanting">鼠标悬停</router-link></a-tag>
+      <a-tag color="red"><router-link to="/shubiaoyidong">鼠标移动</router-link></a-tag>
+      <a-tag color="orange"><router-link to="/wanhuatong">万花筒</router-link></a-tag>
+      <a-tag color="green"><router-link to="/wenzifenge">文字分割</router-link></a-tag>
+      <a-tag color="cyan"><router-link to="/xingxing">星星闪动</router-link></a-tag>
+      <a-tag color="blue"><router-link to="/youling">幽灵特效</router-link></a-tag>
+      <a-tag color="purple"><router-link to="/shanyao">闪耀弹幕</router-link></a-tag>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 <script setup>
 import Test from './views/文字分割.vue'
 </script>
 <style lang="scss">
-body{
+body {
   margin: 0;
+}
+
+.box1111 {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  z-index: 9999999;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
-createApp(App).use(Antd).mount('#app')
+import router from './router/index'
+createApp(App).use(Antd).use(router).mount('#app')
